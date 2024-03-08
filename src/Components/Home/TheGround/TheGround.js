3fb298523197d57fae2b18b13be70e8cfa813249
Syrus Gaddy <gaddys10@gmail.com>
2024-03-08 15:47:48 -0500

    Sorry for not pushing as often

--- src/Components/Home/TheGround/TheGround.js
+++ src/Components/Home/TheGround/TheGround.js
@@ -21,7 +21,62 @@ class TheGround extends React.Component {
                 <div className= "horizonBush8"></div>
                 <div className= "horizonBush9"></div>
                 <div className= "horizonBush10"></div>
+                <div className= "horizonBush11"></div>
+                <div className= "horizonBush12"></div>
+                <div className= "horizonBush13"></div>
+                <div className= "horizonBush14"></div>
+                <div className= "horizonBush15"></div>
+                <div className= "horizonBush16"></div>
+                <div className= "horizonBush17"></div>
+                <div className= "horizonBush18"></div>
+                <div className= "horizonBush19"></div>
+                <div className= "horizonBush10"></div>
+                <div className= "birdShadow"></div>
+                <div className='bush1'>
+                    <div className='bush1part1'></div>
+                    <div className='bush1part2'></div>
+                    <div className='bush1part3'></div>
+                </div>
+                <div className='bush2'>
+                    <div className='bush2part1'></div>
+                    <div className='bush2part2'></div>
+                    <div className='bush2part3'></div>
+                </div>
+                <div className='bush3'>
+                    <div className='bush3part1'></div>
+                    <div className='bush3part2'></div>
+                    <div className='bush3part3'></div>
+                </div>
+                <div className='bush4'>
+                    <div className='bush4part1'></div>
+                    <div className='bush4part2'></div>
+                    <div className='bush4part3'></div>
+                </div>
+                <div className='bush5'>
+                    <div className='bush5part1'></div>
+                    <div className='bush5part2'></div>
+                    <div className='bush5part3'></div>
+                </div>
+
+                <div className='bush6'>
+                    <div className='bush6part1'></div>
+                    <div className='bush6part2'></div>
+                    <div className='bush6part3'></div>
+                </div>
+                <div className='bush7'>
+                    <div className='bush7part1'></div>
+                    <div className='bush7part2'></div>
+                    <div className='bush7part3'></div>
+                </div>
+                <div className='bush8'>
+                    <div className='bush8part1'></div>
+                    <div className='bush8part2'></div>
+                    <div className='bush8part3'></div>
+                </div>
+
+
                 <div id = "grass"></div>
+                
             </div>
         )
     }
